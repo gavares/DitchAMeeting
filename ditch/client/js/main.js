d7b000db0e115ececd36d5e783fb6bd0c1db7c96
Michael M. Wasser <michael@raveld.com>
2013-01-26 18:12:58 -0800

    calls server side method to schedule call and basic validation code

--- ditch/client/js/main.js
+++ ditch/client/js/main.js
@@ -1,3 +1,28 @@
+var handleResult = function (err, result) {
+  if (err) {
+    if (err.details) {
+      var fieldName;
+      for (fieldName in err.details) {
+        var $group = $('#' + fieldName).parent().parent();
+        $group.addClass('error');
+        $group.find('.help-inline').text(err.details[fieldName][0])
+      }
+    } else {
+      $('.text-error').text(err.reason);
+    }
+  } else {
+    $('form').fadeOut(500, function () {
+      $('.scheduled').fadeIn(500);
+      setTimeout(function () {
+        $('.scheduled').fadeOut(500, function () {
+          $('input').val('');
+          $('form').fadeIn(500);
+        });
+      }, 5500);
+    });
+  }
+};
+
 Template.create.events({
   'submit form': function (e) {
     var $target = $(e.target),
@@ -5,7 +30,12 @@ Template.create.events({
 
     e.preventDefault();
 
+    $('.error').removeClass('error');
+    $('.help-inline').text('');
+    $('.text-error').text('');
+
     data = $target.serializeObject();
-    console.log(data);
+
+    Meteor.call('create_phone_call', data, handleResult);
   }
 });
