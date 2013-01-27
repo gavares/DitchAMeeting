e1bc0797c4b1e5b20df58779d201446053a6f618
Grant Gavares <gavares@gmail.com>
2013-01-26 18:09:28 -0800

    Basic impl of create_phone_call finished. (May not run)

--- ditch/ditch.js
+++ ditch/ditch.js
@@ -24,8 +24,15 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+
+  // Max number of allowable retries
+  var maxRetries = 5
+
   // Match a string of 10 digits
   var phoneNumRegex = /^\d{10}$/
+
+  // one week in milliseconds
+  var oneWeek = 1000 * 60 * 60 * 24 * 7;
 
   Meteor.startup(function () {
     // code to run on server at startup
@@ -40,11 +47,42 @@ if (Meteor.isServer) {
         if( phoneCall.phone === undefined ){
           errors.phone.push("No phone number specified.");
         }
-        else if( !phoneNumRegex.match(phoneCall.phone) ){
-          errors.phone.push("Phone number must be 10 digits.")
+        else {
+          phoneCall.phone = phoneCall.phone.trim();
+          if( !phoneNumRegex.match(phoneCall.phone) ){
+            errors.phone.push("Phone number must be 10 digits.")
+          }
         }
 
-        Players.insert(phoneCall)
+        if( phoneCall.time != undefined && phoneCall.time.toLowerCase() != 'now' ){
+          // verify that time is a number and in the future
+          if( typeof phoneCall.time === 'number' ) {
+            var now = new Date().getTime() - 60 * 1000; // some delta to prevent extraneous errors
+            var weekFromNow = now + oneWeek;
+            if( phoneCall.time  < now ){
+              errors.time.push("Scheduled call time cannot be in the past.");
+            }
+            else if( phoneCall.time > weekFromNow ) {
+              errors.time.push("Scheduled call time cannot be more than 1 week in the future.");
+            }
+          }
+          else {
+            throw new Meteor.Error(400, "Call time expected to be a numeric timestamp in milliseconds since epoch.");
+          }
+        }
+
+        if( phoneCall.retries === undefined || phoneCall.retries < 0 ){
+          phoneCall.retries = 0;
+        }
+        else if( phoneCall.retries > maxRetries ){
+          errors.retries.push("Number of retries cannot be more than " + maxRetries);
+        }
+
+        if( errors.phone.length || errors.time.length || errors.retries.length ){
+          throw new Meteor.Error(400, errors);
+        }
+
+        PhoneCalls.insert(phoneCall)
       }
   });
 }
